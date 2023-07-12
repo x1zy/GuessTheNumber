efe44a0b7dd8f12fe45c502bacd367debb866bf2
x1zy <nikitagusev672@gmail.com>
2023-07-12 05:25:26 +0300

    feat: add record saving and a modal window with game reset and results

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,89 @@
 'use strict';
 
-const randomNumber = Math.trunc(Math.random() * 10) + 1;
-let points = 10;
+const originalHearts = document.querySelector('.hearts').innerHTML;
+let randomNumber = Math.trunc(Math.random() * 10) + 1;
+let points = 0;
+let highscore = 0;
+let life = 3;
 
-document.querySelector('.check').addEventListener
-('click', function () {
+function removeLastHeart() {
+    const hearts = document.querySelectorAll('.hearts img.heart');
+    const lastHeart = hearts[hearts.length - 1];
 
+    if (lastHeart) {
+        lastHeart.parentNode.removeChild(lastHeart);
+    }
+}
+
+function showGameOverModal() {
+    const modal = document.querySelector('.modal');
+    const modalScore = document.querySelector('.modal-score');
+    const modalHighscore = document.querySelector('.modal-highscore');
+
+    modalScore.textContent = points;
+    modalHighscore.textContent = highscore;
+    modal.style.display = 'block';
+}
+
+function resetGame() {
+    randomNumber = Math.trunc(Math.random() * 10) + 1;
+    points = 0;
+    life = 3;
+
+    document.querySelector('.score').textContent = points;
+    document.querySelector('.highscore').textContent = highscore;
+    document.querySelector('.guess-message').textContent = 'Start guessing!';
+    document.querySelector('.number-input').value = '';
+
+    const heartContainer = document.querySelector('.hearts');
+    heartContainer.innerHTML = originalHearts;
+}
+
+
+document.querySelector('.check').addEventListener('click', function() {
     const guessingNumber = Number(document.querySelector('.number-input').value);
 
     // No Input
     if (!guessingNumber) {
-
-        document.querySelector('.guess-message').
-        textContent = 'Input some number!';
+        document.querySelector('.guess-message').textContent = 'Input some number!';
+        return;
+    }
 
     // Player won
-    } else if (guessingNumber === randomNumber) {
-
-        document.querySelector('.guess-message').
-        textContent = 'Right!';
+    if (guessingNumber === randomNumber) {
+        document.querySelector('.guess-message').textContent = 'Right!';
         points++;
         document.querySelector('.score').textContent = points;
-
-    // Too High
-    } else if (guessingNumber > randomNumber) {
-
-        if (points > 1) {
-            document.querySelector('.guess-message').textContent =
-            'Too much';
-            points--;
-            document.querySelector('.score').textContent = points;
-        } else {
-            document.querySelector('.guess-message').textContent =
-            'Game Over!';
+        if (points > highscore) {
+            highscore = points;
+            document.querySelector('.highscore').textContent = highscore;
         }
+        return;
+    }
 
-    // Too low
-    } else if (guessingNumber < randomNumber) {
+    // Too High or Too Low
+    document.querySelector('.guess-message').textContent = (guessingNumber > randomNumber) ? 'Too much' : 'Too few';
+        life--;
+        points--;
+    document.querySelector('.score').textContent = points;
 
-        if (points > 1) {
-            document.querySelector('.guess-message').textContent =
-            'Too few';
-            points--;
-            document.querySelector('.score').textContent = points;
-        } else {
-            document.querySelector('.guess-message').textContent =
-            'Game Over!';
-            document.querySelector('.score').textContent = 0;
-        }
-
+    if (life > 0) {
+        removeLastHeart();
+    } else {
+        document.querySelector('.guess-message').textContent = 'Game Over!';
+        document.querySelector('.score').textContent = points;
+        removeLastHeart();
+        showGameOverModal();
     }
 });
+
+document.querySelector('.modal-replay').addEventListener('click', function() {
+    resetGame();
+    document.querySelector('.modal').style.display = 'none';
+});
+
+resetGame();
+
+
+
 
